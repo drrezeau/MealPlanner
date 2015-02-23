95c6ad9f3354cfc7a51634ef90547445d4c0964b
David <david.r.lambertson@gmail.com>
2015-02-23 08:35:44 -0700

    Hi peeps

--- app/src/main/java/com/example/gil/mealplanner/MainActivity.java
+++ app/src/main/java/com/example/gil/mealplanner/MainActivity.java
@@ -1,16 +1,46 @@
 package com.example.gil.mealplanner;
 
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CalendarView;
+import android.widget.Toast;
+
+//import java.util.Calendar;
 
 public class MainActivity extends ActionBarActivity {
 
+    CalendarView cal;
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cal = (CalendarView) findViewById(R.id.calendarView);
+
+        cal.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month,
+                                            int dayOfMonth) {
+//                TODO Auto-generated method stub
+
+                Toast.makeText(getBaseContext(), "Selected Date is\n\n"
+                                + dayOfMonth + " : " + month + " : " + year,
+                        Toast.LENGTH_LONG).show();
+                Context context = cal.getContext();
+
+                Intent intent = new Intent(context, dayView.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
